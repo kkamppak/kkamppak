42b16e22a6785467f5e65eac17c07dc399958d40
김다빈 <mokdb17@gmail.com>
2024-07-14 14:42:12 +0900

    [Setting/#1] Create Project

---
+++ KKAMPPAK/KKAMPPAK/KKAMPPAKApp.swift
@@ -0,0 +1,17 @@
+//
+//  KKAMPPAKApp.swift
+//  KKAMPPAK
+//
+//  Created by 김다빈 on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct KKAMPPAKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
